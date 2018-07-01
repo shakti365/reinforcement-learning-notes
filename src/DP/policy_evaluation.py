5878a98a1a7df121331b0a1444825365998f8cd5
Shivam Shakti <shivam.shakti365@gmail.com>
2018-07-01 23:56:23 +0530

    corrected stopping criteria implementation

--- src/DP/policy_evaluation.py
+++ src/DP/policy_evaluation.py
@@ -25,16 +25,37 @@ class PolicyEvaluation:
 
         # Run policy evaluation.
         while True:
+
+            # Initialize stopping threshold.
             threshold = 0
+
+            # Create copy of old value functions for all states.
             v = self.v.copy()
+
+            # Iterate over all the states in state space.
             for state in self.environment.state_space:
+
+                # Initialize temporary variable to calculate value function for current state
                 value_temp = 0
+
+                # Iterate over all possible actions from current state.
                 for action_idx, action in enumerate(self.environment.action_space):
+
+                    # Perform action in the environment and observe reward and next state.
                     reward, next_state = self.environment.step(action=action, state=state)
-                    value_temp += self.policy[state, action_idx] * (reward + (discount * v[next_state]))
+
+                    # Calculate value function for the state given the current reward and policy
+                    value_temp += self.policy[state, action_idx] * (reward + (discount * self.v[next_state]))
+
+                # Assign temporary value to value function of current state.
                 self.v[state] = value_temp
-                threshold = max(threshold,np.max(np.abs(v - self.v[state])))
+
+            # Calculate max change in value functions from last state
+            threshold = max(threshold, np.max(np.abs(v - self.v)))
+            
             print self.v, threshold
+            
+            # Check if change in value function from last state to current is above the tolerance value and stop iteration.
             if threshold < theta:
                 break
             
