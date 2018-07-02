78e66007199b87510a45e1553d52c58b6a96b740
Shivam Shakti <shivam.shakti365@gmail.com>
2018-07-02 23:58:01 +0530

    refactored policy evaluation to separate function

--- src/DP/policy_evaluation.py
+++ src/DP/policy_evaluation.py
@@ -21,6 +21,29 @@ class PolicyEvaluation:
         # Initialize variables for policy evaluation.
         self.v = np.zeros(self.environment.num_states)
 
+    def policy_evaluation(self, discount):
+        """
+        Run policy evaluation for one step.
+        """
+        # Iterate over all the states in state space.
+        for state in self.environment.state_space:
+
+            # Initialize temporary variable to calculate value function for current state
+            value_temp = 0
+
+            # Iterate over all possible actions from current state.
+            for action_idx, action in enumerate(self.environment.action_space):
+
+                # Perform action in the environment and observe reward and next state.
+                reward, next_state = self.environment.step(action=action, state=state)
+
+                # Calculate value function for the state given the current reward and policy
+                value_temp += self.policy[state, action_idx] * (reward + (discount * self.v[next_state]))
+
+            # Assign temporary value to value function of current state.
+            self.v[state] = value_temp
+
+
     def run(self, discount=1.0, theta=0.00001):
 
         # Run policy evaluation.
@@ -32,23 +55,8 @@ class PolicyEvaluation:
             # Create copy of old value functions for all states.
             v = self.v.copy()
 
-            # Iterate over all the states in state space.
-            for state in self.environment.state_space:
-
-                # Initialize temporary variable to calculate value function for current state
-                value_temp = 0
-
-                # Iterate over all possible actions from current state.
-                for action_idx, action in enumerate(self.environment.action_space):
-
-                    # Perform action in the environment and observe reward and next state.
-                    reward, next_state = self.environment.step(action=action, state=state)
-
-                    # Calculate value function for the state given the current reward and policy
-                    value_temp += self.policy[state, action_idx] * (reward + (discount * self.v[next_state]))
-
-                # Assign temporary value to value function of current state.
-                self.v[state] = value_temp
+            # Call policy evaluation.
+            self.policy_evaluation(discount)
 
             # Calculate max change in value functions from last state
             threshold = max(threshold, np.max(np.abs(v - self.v)))
